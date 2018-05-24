8247299abc03f5a2e434262d6f6aa30a115c7d54
Adrian Rodriguez <adrian.rodriguez7109@comunidad.unam.mx>
2018-05-24 17:27:14 -0500

    Registro

--- formulario.php
+++ formulario.php
@@ -1,4 +1,6 @@
 <?php
+	include('./cifrado.php');
+
 	function val($r,$s,$n){//funcion para validacion
 		if (!preg_match($r,$s)){
 			echo "Formato de $n invalido <br/>";
@@ -32,10 +34,26 @@
 	}
 	$e += val ("/[A-Za-z0-9._%+-]/",$usu,'Usuario');
 	$e += val ("/([A-Za-z0-9]+|[\.]|[\=]|[\)]|[\(]|[\/]|[\&]|[\%]|[\$]|[\#]|[\!]|[\*]|[\+]|[\´]|[\~]|[\#]|[\_]|[\+]|[\{]|[\}]|[\|]|[\[]|[\]]|[\;]|[\:]|[\"]|[\<]|[\>]|[\?]|[\,][^(13245)|(qwert)|(asdfg)|(zxcv)|aaa|ooo|eee|iii|uuu][^ñ|á|í|ó|ú|ü|ö|Á|É|Í|Ó|Ú|Ü|Ö])+/",$pas,'Password');
-
 	 if ($e==0){
-		 echo "<a href='contraseñas.html'>Registrar contraseñas</a>";//pagina para registra contraseñas
-	 }else{
+		$usuario = $usu;
+		$nombre = cifrar($nom, $usuario);
+		$ap_pat = cifrar($pat, $usuario);
+		$ap_mat = cifrar($mat, $usuario);
+		$lada = cifrar($lada, $usuario);
+		$telefono = cifrar($tel, $usuario);
+		$email = cifrar($em, $usuario);
+		$contrasenia = md5($pas);
+
+		echo $nombre . "<br>";
+		$query = "INSERT INTO usuario(nombre, apellido_paterno, apellido_materno, lada, telefono, email, usuario, contraseña)";
+		$query = $query .    " VALUES($nombre, $ap_pat, $ap_mat, $lada, $telefono, $email, $usuario, $contrasenia)";
+
+		echo $query;
+
+		setcookie(md5("usuario"), cifrar($usuario, "cookie"), 0, "/");
+		header("Location: ./");
+
+	}else{
 		 echo "<a href='formulario.html'>Regresar al formulario</a>";
 	 }
 
